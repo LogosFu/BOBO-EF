2bc3d5005638f1b7a11e2fc1386cd2dc5403d743
Bo Zhao <BZhao7@slb.com>
2022-11-03 16:03:32 +0800

    feat: Add Employee Entity

--- EFCoreRelationshipsPractice/Dtos/CompanyDto.cs
+++ EFCoreRelationshipsPractice/Dtos/CompanyDto.cs
@@ -13,13 +13,14 @@ namespace EFCoreRelationshipsPractice.Dtos
         {
             Name = companyEntity.Name;
             ProfileDto = companyEntity.Profile != null ? new ProfileDto(companyEntity.Profile) : null;
+            EmployeesDto = companyEntity.Employees?.Select(employee => new EmployeeDto(employee)).ToList();
         }
 
         public string Name { get; set; }
 
         public ProfileDto? ProfileDto { get; set; }
 
-        public List<EmployeeDto>? Employees { get; set; }
+        public List<EmployeeDto>? EmployeesDto { get; set; }
 
         public CompanyEntity ToEntity()
         {
@@ -27,6 +28,7 @@ namespace EFCoreRelationshipsPractice.Dtos
             {
                 Name = Name,
                 Profile = this.ProfileDto?.ToEntity(),
+                Employees = this.EmployeesDto?.Select(employee => employee.ToEntity()).ToList(),
             };
         }
     }
